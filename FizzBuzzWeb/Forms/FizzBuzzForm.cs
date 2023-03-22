c271e402ec804bc77af9946162e2a6d3d0c38898
Kacper Bochan <kacperbochan@gmail.com>
2023-03-22 14:35:56 +0100

    FizzBuzz word

---
+++ FizzBuzzWeb/Forms/FizzBuzzForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzzForm
+    {
+        [Display(Name = "PIN")]
+        [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}."), ]
+        public int? Number { get; set; }
+    }
+}
--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FizzBuzzWeb.Pages
@@ -7,6 +8,13 @@ namespace FizzBuzzWeb.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public string Word { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +22,43 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
+            Word = "";
+            CheckUserName();
+        }
 
+        public IActionResult OnPost()
+        {
+            CheckUserName();
+            WordBuzz();
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+        private void CheckUserName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        private void WordBuzz() {
+            if (FizzBuzz.Number is not null)
+            {
+                Word = "";
+                if (FizzBuzz.Number % 3 == 0)
+                {
+                    Word += "Fizz";
+                }
+                if (FizzBuzz.Number % 5 == 0)
+                {
+                    Word += "Buzz";
+                }
+            }
         }
     }
 }
